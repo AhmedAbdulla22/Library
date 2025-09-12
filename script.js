88b236efd995563fa3bc95f163d1bcbc28a440ad
AhmedAbdulla22 <ahmeddev452@gmail.com>
2025-09-12 17:56:41 +0300

    add array library, constructor, addNewBook method

---
+++ script.js
@@ -0,0 +1,24 @@
+const myLibrary = [];
+
+
+function Book(name,pages,status) {
+
+    if(!new.target) {
+        throw Error("You Must include new when create new Obj!");
+    }
+
+    this.name = name;
+    this.pages = pages;
+    this.status = status;
+    this.info = function () {
+        let info = this.name + ', ' + this.pages + ' pages' + ', ' + (this.status ? "completed":"not read yet"); 
+        return info;
+    }
+}
+
+function addNewBook(name,pages,status) {
+    const newBook = new Book(name,pages,status);
+    
+    //add new book to the library
+    myLibrary.push(newBook);
+}
